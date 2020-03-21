48e084b0b055eac3502f5a039190d5071b0a912b
Ashutosh-Rao <60297156+Ashutosh-Rao@users.noreply.github.com>
2020-03-21 11:44:38 +0530

    REVERSE FOR CIPHERS
    Reversing is done for both. Let me know if any issue arises.

--- cypher.c
+++ cypher.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 #include<string.h>
 
+
 char * encrypt(char *, int);
+
 
 char * decrypt(char *, int);
 
-int main(){
+
+int main()
+{
     char test[] = "BIGOTED";
     char test2[] = "M KS XS EDOEFER";
     printf("%s\n",encrypt(test,5));
@@ -13,34 +17,73 @@ int main(){
     return 0;
 }
 
-char * encrypt(char *message, int shift_key){
+
+
+char * encrypt(char *message, int shift_key)
+{
+    int begin=0, char s , count=0;
     char *index = message;
-    while(*index != '\0'){
-        if(*index == ' '){
+    while(*index != '\0')
+    {
+        if(*index == ' ')
+        {
             index++;
             continue;
         }
-        if(*index + shift_key > 'Z'){
+        if(*index + shift_key > 'Z')
+        {
             *index = *index + shift_key-26;
-        } else *index = *index+shift_key;
+        } 
+        else *index = *index+shift_key;
         index++;
-    }
-    return message;
+    } 
+    
+   s=message;
+   while (s[count] != '\0')
+   count++;
+   end = count - 1;
+   for (begin = 0; begin < count; begin++) 
+   {
+      message[begin] = s[end];
+      end--;
+   }
+   message[begin] = '\0';
+   return message;
 }
 
-char * decrypt(char *message, int shift_key){
+
+
+char * decrypt(char *message, int shift_key)
+{
+    int begin=0, char s , count=0;
     char *index = message;
-    while(*index != '\0'){
-        if(*index == ' '){
+    while(*index != '\0')
+    {
+        if(*index == ' ')
+        {
             index++;
             continue;
         }
-        if(*index - shift_key < 'A'){
+        if(*index - shift_key < 'A')
+        {
             *index = *index - shift_key+26;
-        } else *index = *index-shift_key;
+        }
+        else *index = *index-shift_key;
         index++;
     }
-    return message;
+    
+   s=message;
+   while (s[count] != '\0')
+   count++;
+   end = count - 1;
+   for (begin = 0; begin < count; begin++) 
+   {
+      message[begin] = s[end];
+       end--;
+   }
+  message[begin] = '\0';
+  return message;    
+   
 }
 
 
